7bed1d7300f9da540a1fb7e3ceb83520b1c8a42d
Meta-Sean <seanhuckabee@gmail.com>
2023-10-19 14:36:19 -0500

    mistral hf integration

---
+++ app/api/chat/old_route.ts
@@ -0,0 +1,76 @@
+import { kv } from '@vercel/kv'
+import { OpenAIStream, StreamingTextResponse } from 'ai'
+import { Configuration, OpenAIApi } from 'openai-edge'
+
+import { auth } from '@/auth'
+import { nanoid } from '@/lib/utils'
+
+export const runtime = 'edge'
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+})
+
+const openai = new OpenAIApi(configuration)
+
+export async function POST(req: Request) {
+    const json = await req.json()
+    const { messages, previewToken } = json
+    const userId = (await auth())?.user.id
+
+    if (!userId) {
+        return new Response('Unauthorized', {
+            status: 401
+        })
+    }
+
+    if (previewToken) {
+        configuration.apiKey = previewToken
+    }
+
+    const systemMessage = {
+        role: "system",
+        content: "You are a helpful and knowledgeable teacher, Socrates, begin by asking what subject the user would like to learn then use the Socratic method to help distill the knowledge. Ask questions about the topic and see if they can answer correctly and correct any mistakes"
+    };
+
+    messages.unshift(systemMessage);
+    console.log(messages)
+
+
+
+    const res = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages,
+        temperature: 0.7,
+        stream: true
+    })
+
+    const stream = OpenAIStream(res, {
+        async onCompletion(completion) {
+            const title = json.messages[0].content.substring(0, 100)
+            const id = json.id ?? nanoid()
+            const createdAt = Date.now()
+            const path = `/chat/${id}`
+            const payload = {
+                id,
+                title,
+                userId,
+                createdAt,
+                path,
+                messages: [
+                    ...messages,
+                    {
+                        content: completion,
+                        role: 'assistant'
+                    }
+                ]
+            }
+            await kv.hmset(`chat:${id}`, payload)
+            await kv.zadd(`user:chat:${userId}`, {
+                score: createdAt,
+                member: `chat:${id}`
+            })
+        }
+    })
+
+    return new StreamingTextResponse(stream)
--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,31 +1,23 @@
-import { kv } from '@vercel/kv'
-import { OpenAIStream, StreamingTextResponse } from 'ai'
-import { Configuration, OpenAIApi } from 'openai-edge'
+import { kv } from '@vercel/kv';
+import { HfInference } from '@huggingface/inference';
+import { HuggingFaceStream, StreamingTextResponse } from 'ai';
 
-import { auth } from '@/auth'
-import { nanoid } from '@/lib/utils'
+import { auth } from '@/auth';
+import { nanoid } from '@/lib/utils';
 
-export const runtime = 'edge'
+export const runtime = 'edge';
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY
-})
-
-const openai = new OpenAIApi(configuration)
+const Hf = new HfInference(process.env.HUGGINGFACE_API_KEY);
 
 export async function POST(req: Request) {
-  const json = await req.json()
-  const { messages, previewToken } = json
-  const userId = (await auth())?.user.id
+  const json = await req.json();
+  const { messages } = json;
+  const userId = (await auth())?.user.id;
 
   if (!userId) {
     return new Response('Unauthorized', {
       status: 401
-    })
-  }
-
-  if (previewToken) {
-    configuration.apiKey = previewToken
+    });
   }
 
   const systemMessage = {
@@ -34,23 +26,27 @@ export async function POST(req: Request) {
   };
 
   messages.unshift(systemMessage);
-  console.log(messages)
+  console.log(messages);
 
+  const prompt = messages.map(msg => `<s>[INST]${msg.role}: ${msg.content}[/INST]`).join('\n');
+  const response = await Hf.textGenerationStream({
+    model: "mistralai/Mistral-7B-Instruct-v0.1",
+    inputs: prompt,
+    parameters: {
+      max_new_tokens: 200,
+      typical_p: 0.2,
+      repetition_penalty: 1,
+      truncate: 1000,
+      return_full_text: false,
+    },
+  });
 
-
-  const res = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    messages,
-    temperature: 0.7,
-    stream: true
-  })
-
-  const stream = OpenAIStream(res, {
-    async onCompletion(completion) {
-      const title = json.messages[0].content.substring(0, 100)
-      const id = json.id ?? nanoid()
-      const createdAt = Date.now()
-      const path = `/chat/${id}`
+  const stream = HuggingFaceStream(response, {
+    async onCompletion(completion: string) {
+      const title = json.messages[0].content.substring(0, 100);
+      const id = json.id ?? nanoid();
+      const createdAt = Date.now();
+      const path = `/chat/${id}`;
       const payload = {
         id,
         title,
@@ -64,14 +60,14 @@ export async function POST(req: Request) {
             role: 'assistant'
           }
         ]
-      }
-      await kv.hmset(`chat:${id}`, payload)
+      };
+      await kv.hmset(`chat:${id}`, payload);
       await kv.zadd(`user:chat:${userId}`, {
         score: createdAt,
         member: `chat:${id}`
-      })
+      });
     }
-  })
+  });
 
-  return new StreamingTextResponse(stream)
+  return new StreamingTextResponse(stream);
 }
